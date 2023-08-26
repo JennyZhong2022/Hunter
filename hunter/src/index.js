a303a028a579cfa13e7513381372807e2ba77bd7
jennyz <zhongxiao2017@gmail.com>
2023-08-27 00:52:56 +1000

    update index with EmailJS

--- hunter/src/index.js
+++ hunter/src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import emailjs from "emailjs-com";
+
+// Initialize EmailJS
+emailjs.init(process.env.REACT_APP_EMAILJS_USER_ID);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
