6c020bfee180e1870d48b19910220e049f16106d
jennyz <zhongxiao2017@gmail.com>
2023-08-27 10:50:40 +1000

    update index.js

--- hunter/src/index.js
+++ hunter/src/index.js
@@ -6,6 +6,7 @@ import emailjs from "emailjs-com";
 
 // Initialize EmailJS
 emailjs.init(process.env.REACT_APP_EMAILJS_USER_ID);
+console.log(process.env.REACT_APP_EMAILJS_USER_ID);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
